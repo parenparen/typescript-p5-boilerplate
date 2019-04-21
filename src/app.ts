9e9afd67d20138e17fafa554eb62e691537d02b3
parenparen <dtmuelle@gmail.com>
2019-04-21 15:14:10 -0700

    random noise experiment

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
 import p5 from 'p5';
 require('../assets/index.scss');
 
+function noise(p: p5, val: number): number {
+  return (p.noise(val / 50) - 0.5) * 50;
+}
+
+function customNoise0(p: p5, val: number): number {
+  return Math.sin(val / 5) * 10;
+}
+
+function drawLineWithNoise(p: p5, offset: number, noise: (p: p5, val: number) => number): void {
+  for(let i = 0; i < window.innerWidth; ++i) {
+    p.circle(i, offset + noise(p, i), 5);
+  }
+}
+
 const app = () => {
   new p5((p: p5) => {
     p.setup = () => {
       p.createCanvas(window.innerWidth, window.innerHeight);
-      p.background("blue");
+      p.background("gray");
+      p.noLoop();
     };
 
     p.draw = () => {
-      p.rect(
-        window.innerWidth / 2 - 25, window.innerHeight / 2 - 25, 50, 
-        50);
+      drawLineWithNoise(p, window.innerHeight / 2, noise);
+      drawLineWithNoise(p, window.innerHeight / 4, customNoise0);
+      drawLineWithNoise(p, window.innerHeight / 4 * 3, noise);
     };
   });
 };
