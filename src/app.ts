9414c30cbeff9d48ccd2bb1a6390c0fc2651b60e
parenparen <dtmuelle@gmail.com>
2019-05-18 20:39:56 -0700

    add spirals

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,27 @@ require('../assets/index.scss');
 
 const p = getP5Singleton();
 
+function drawSpiral0({cx, cy} : {cx: number, cy: number}): void {
+  let maxRadians = 10 * Math.PI,
+    r = 1;
+  for(let a = 0; a < maxRadians; a += Math.PI / 150) {
+    r += 0.1;
+    p.circle(
+      cx + r * Math.cos(a), 
+      cy + r *  Math.sin(a), 5);
+  }
+}
+
+function drawSpiral1({cx, cy} : {cx: number, cy: number}): void {
+  let maxRadians = 10 * Math.PI,
+    r = 5;
+  for(let a = 0; a > -maxRadians; a -= Math.PI / 150) {
+    p.circle(
+      cx + r * a * Math.cos(a), 
+      cy + r * a * Math.sin(a), 5);
+  }
+}
+
 const app = () => {
   let refresh = !!p.setup;
 
@@ -13,9 +34,10 @@ const app = () => {
   };
 
   p.draw = () => {
-    p.rect(
-      window.innerWidth / 2 - 25, window.innerHeight / 2 - 25, 50, 
-      50);
+    drawSpiral0(
+      {cx: window.innerWidth / 4 * 3, cy: window.innerHeight / 2});
+    drawSpiral1(
+      {cx: window.innerWidth / 4 * 1, cy: window.innerHeight / 2});
   };
 
   if(refresh) {
