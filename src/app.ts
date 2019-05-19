912678f16f1cbac55544829a82b401918b3cdd9a
parenparen <dtmuelle@gmail.com>
2019-05-18 20:10:13 -0700

    add more custom noise

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,26 @@
-import getP5Singleton, {p5} from './p5Singleton.ts';
+import getP5Singleton from './p5Singleton.ts';
 require('../assets/index.scss');
 
 const p = getP5Singleton();
 
 function noise(val: number): number {
-  return (p.noise(val / 50) - 0.5) * 50;
+  return (p.noise(val / 100) - 0.5) * 50;
 }
 
 function customNoise0(val: number): number {
-  return Math.sin(val / 5) * 10;
+  return Math.sin(val / 20) * 9;
+}
+
+function customRandom(val: number): number {
+  return 20 * Math.pow(p.random(-1, 1), 1 / 9);
 }
 
 function drawLineWithNoise(
   offset: number, 
   noise: (val: number) => number): void {
 
-  for(let i = 0; i < window.innerWidth; ++i) {
-    p.circle(i, offset + noise(i), 5);
+  for(let i = 0; i < window.innerWidth; i += 1) {
+    p.circle(i, offset + noise(i), 3);
   }
 }
 
@@ -36,6 +40,10 @@ const app = () => {
       (i) => noise(i) + customNoise0(i));
     drawLineWithNoise(window.innerHeight / 8 * 4, 
       (i) => noise(i) / 8 * customNoise0(i));
+    drawLineWithNoise(window.innerHeight / 8 * 5, 
+      (i) => -Math.abs(noise(i) / 4 * customNoise0(i)));
+    drawLineWithNoise(window.innerHeight / 8 * 6,
+      customRandom); 
   };
 
   if(refresh) {
